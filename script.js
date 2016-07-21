b1f42775e37cf481ba7994988bdf790bde3934e2
Jacob Stoebel <stoebelj@berea.edu>
2016-07-21 09:11:17 -0400

    begining of work on step method

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var Cell = React.createClass({
 
   getInitialState: function(){
-    return {"live": this.props.startValue}
+    return {live: this.props.startValue}
   },
 
   componentWillMount: function(){
@@ -15,7 +15,7 @@ var Cell = React.createClass({
   },
 
   toggleState: function(){
-    this.setState({"live" : !this.state.live})
+    this.setState({ live : !this.state.live})
   },
 
   render: function(){
@@ -38,38 +38,61 @@ var Cell = React.createClass({
 
 var Board = React.createClass({
 
-  eachCell: function(startValue, cell_index){
-    var cellId = "cell"+cell_index
-    return  (
-      <Cell
-        key={cellId}
-        startValue={startValue}>
-      </Cell>
-    )
+  getInitialState: function(){
+    var cells = this.props.structure.map(function(row, row_i){
+      return row.map(function(cellValue, col_i){
+        return (
+          <Cell
+          key={row_i+"-"+col_i}
+          startValue={cellValue} />
+        )
+      })
+    })
+    return {'cells': cells}
+  },
+
+  step: function(){
+    //one step of the game
+
+    function getNeighboors(row, column){
+      //row(int): a row index
+      //column(int): a column index
+      //returns an array of all of the neighboors to the cell at coordinates row, column
+
+      var neighboors = [];
+      for(var r=row-1; r<row+2; r++){
+        for(var c=column-1; c<column+2; c++){
+          neighboors.push(this.state.cells[r][c])
+        }
+      }
+      return neighboors;
+    }
+
+    return getNeighboors(0, 0).bind(this);
+
+  },
+
+  componentDidMount: function(){
+    // this.state.cells.map(function(row, row_i){
+    //   console.log(row)
+    // })
+
+    console.log(this.step())
 
   },
 
   render: function(){
 
-    var cells = this.props.structure.map(function(row, row_i){
-      var cell = row.map(function(cellValue, col_i){
-        return (
-          <Cell
-            key={row_i+col_i}
-            startValue={cellValue} />
-        )
-      })
-    })
+
 
     return (
       <div className="container">
         <div className="panel-group" id="accordion">
-          {cells}
+          {this.state.cells}
         </div>
       </div>
     )
   }
-
 })
 
 var cells = [];
@@ -77,7 +100,7 @@ var cells = [];
 for (var h=0; h<25; h++){
   var cell_row = [];
   for (var w=0; w<50;  w++){
-    cell_row.push(Math.round(Math.random()));
+    cell_row.push(Boolean(Math.round(Math.random())));
   }
     cells.push(cell_row);
 }
